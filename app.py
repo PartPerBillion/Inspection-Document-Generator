b9fba295ad7f1f1ade7a0a82a729de056f59911f
Srihari P <92159097+PartPerBillion@users.noreply.github.com>
2023-11-25 17:00:40 +0530

    Update app.py
    commented a few more

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # from PIL import Image
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from datetime import date
 
 
