2483ab577f78776745038627f545f812f5e64e64
PartPerBillion <srihari1265@gmail.com>
2023-12-19 19:13:12 +0530

    update as per requirements

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import pandas as pd
 from datetime import date
 import streamlit as st
+from docxtpl import DocxTemplate
+from docx import Document
+import lxml
 from make_document import make_inspection_document
+from streamlit_gsheets import GSheetsConnection
 
+conn = st.connection("gsheets", type=GSheetsConnection)
+data = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1DJXGDW3WJbhsHxBAQ6YtZMEQqp62keCe3QPGd7jRZDs/edit?usp=sharing", usecols=[0, 1,2])
+ccode = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1DJXGDW3WJbhsHxBAQ6YtZMEQqp62keCe3QPGd7jRZDs/edit?usp=sharing", usecols=[0, 1], worksheet='1362423819')
+
+def set_updatefields_true(docx_path):
+    namespace = "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}"
+    doc = Document(docx_path)
+    # add child to doc.settings element
+    element_updatefields = lxml.etree.SubElement(
+        doc.settings.element, f"{namespace}updateFields"
+    )
+    element_updatefields.set(f"{namespace}val", "true")
+    doc.save(docx_path)## Heading ##
 
 st.title(":rainbow[Inspection Report Generator]")
 
@@ -11,11 +28,15 @@ st.header(':blue[Info]', divider='violet')
 # client
 st.subheader(':grey[Client]')#, divider='grey')
 c1,c2 = st.columns([4,1])   
-client_name = c1.text_input("Client name:")
-report_number = c2.number_input('Report number:',min_value = 0)
+clients = tuple(data['Client'].unique().tolist())
+client_name = c1.selectbox('Client name:',clients)
+clientcode = ccode[ccode['Client']==client_name]['Code'][0]
+client_code = c2.text_input('Client code:',clientcode)
 c3,c4 = st.columns([4,1])
-client_location = c3.text_input("Client location:")
-unit_number = c4.number_input('Unit number:',min_value = 0)
+clientlocation = tuple(data[data['Client']==client_name]['Location'].unique().tolist())
+client_location = c3.selectbox('Client location:',clientlocation)
+unitnumber = tuple(data[(data['Client']==client_name)&(data['Location']==client_location)]['Unit'].tolist())
+unit_number = c4.selectbox('Unit number:',unitnumber)
 client_logo = st.file_uploader("Upload Client logo:", type=['png','jpeg','jpg'], accept_multiple_files=False)
 # inspection
 st.subheader(':grey[Inspection]')#, divider='grey')
@@ -25,7 +46,7 @@ inspection_type = c5.text_input('Inspection type:')
 # st.subheader('Equipment')#, divider='grey')
 c7,c8 = st.columns([4,1])
 equipment_name = c7.text_input('Equipment name:')
-tag_number = c8.number_input('Tag number:',min_value = 0)
+tag_number = c8.text_input('Tag number:')
 # Prepared by
 st.subheader(':grey[Authors]')#, divider='grey')
 df = pd.DataFrame(
@@ -63,16 +84,24 @@ st.subheader(':grey[Tower drawings and scanning location]')#,divider='red')
 tower_drawing = st.file_uploader("Upload Tower drawings and scanning location pictures:", type=['png','jpeg','jpg'], accept_multiple_files=True)
 if tower_drawing is not None:
     st.image(tower_drawing,width=233)
-# st.subheader(':grey[Detailed reports]')
+
 st.subheader(':grey[Shell plate pictures]')#,divider='red')
 shell_plate_pics = st.file_uploader("Upload Shell plate pictures:", type=['png','jpeg','jpg'], accept_multiple_files=True)
 if shell_plate_pics is not None:
     st.image(shell_plate_pics,width=233)
+
 st.subheader(':grey[Detailed reports]')
-detailed_report = st.file_uploader("Upload detailed report files:", type=['csv'], accept_multiple_files=True)
+result = st.number_input('Number of Sections',min_value=1)
+detailed_report = {}
+for i in range(result):
+    section_name = st.text_input(f'Section Title {i+1}:')
+    section = st.file_uploader(f"Upload detailed report files {i+1}:", type=['csv'], accept_multiple_files=True)
+    detailed_report[section_name] = section
 st.divider()
 
-filename = f"{client_name}-{report_number}.docx"
-doc = make_inspection_document(client_name, client_location, unit_number, report_number, inspection_date, equipment_name, inspection_type, edited_df, result_and_conclusion, site_observation, overall_summary, thickness_details, scanning_details, shellwise_inspection, tower_drawing, shell_plate_pics, detailed_report)
+filename = f"document.docx"
+doc = make_inspection_document(client_name, client_location, unit_number, client_code, inspection_date, equipment_name, tag_number, inspection_type, edited_df, result_and_conclusion, site_observation, overall_summary, thickness_details, scanning_details, shellwise_inspection, tower_drawing, shell_plate_pics, detailed_report)
 doc.save(rf'Temp\{filename}')
+
+set_updatefields_true(rf'Temp\{filename}')
 st.download_button("Generate report" , data=open(rf'Temp\{filename}', "rb").read(), file_name=filename, use_container_width = True, mime = "application/octet-stream" )
--- make_document.py
+++ make_document.py
@@ -3,9 +3,16 @@ import docx
 import numpy as np
 import streamlit as st
 from PIL import Image
-from docx.shared import Inches, Pt
+from docx.shared import Inches, Pt, Mm
 from docx.oxml import OxmlElement, ns
 from docxtpl import DocxTemplate
+
+def line_space(doc, number_of_lines):
+    for i in range(number_of_lines):
+        p = doc.add_paragraph()
+        p.style = None
+        p.add_run(style = None)
+        p.add_run(style = None)
 
 def heading_number_generaator(heading, sub_heading, sub_sub_heading):
     if sub_heading == 0:
@@ -57,13 +64,24 @@ def add_site_observation_to_doc(string,doc,hn):
         if len(v)>0:
             if k[-1] == 'h':
                 hn = ahn(hn,2)
-                doc.add_heading(f'{hn} {v}',2)
+                doc.add_heading(f'{hn}. {v}',2)
+                line_space(doc,1)
             elif k[-1] == 's':
                 hn = ahn(hn,3)
-                doc.add_heading(f'{hn} {v}',3)
+                doc.add_heading(f'{hn}. {v}',3)
+                line_space(doc,1)
             else:
-                p = doc.add_paragraph()
-                p.add_run(v)
+                current_point = ''
+                line = v.strip()
+                if line.startswith('-'):
+                    if current_point:
+                        doc.add_paragraph(current_point.strip()[2:], style = 'List Bullet')
+                        line_space(doc,1)
+                        current_point =  ''
+                current_point += ' '+line
+                if current_point:
+                    doc.add_paragraph(current_point.strip()[2:], style = 'List Bullet')
+                    line_space(doc,1)
     return hn
 
 def table_of_contents(document):
@@ -107,17 +125,46 @@ def add_page_number(run):
     run._r.append(instrText)
     run._r.append(fldChar2)
 
-def add_table_to_document(df, document):
+def add_table_to_document(df, document, style = 'Table Grid'):
     if len(df) > 0:
         table = document.add_table(rows=df.shape[0] + 1, cols=df.shape[1])
-        table.style = 'Table Grid'
+        table.style = style
         for j, column in enumerate(df.columns):
             table.cell(0, j).text = column
         for i, row in df.iterrows():
             for j, value in enumerate(row):
                 table.cell(i + 1, j).text = str(value)
 
-def make_inspection_document(client_name, client_location, unit_number, report_number, inspection_date, equipment_name, inspection_type, edited_df, result_and_conclusion, site_observation, overall_summary, thickness_details, scanning_details, shellwise_inspection, tower_drawing, shell_plate_pics, detailed_report):
+def update_docx_fields(doc):
+    try:
+        for field in doc.inline_shapes:
+            if field._inline.graphic.graphicData.xml.startswith('<w:drawing'):
+                # Update specific fields as needed, this example updates all date fields
+                if field._inline.graphic.graphicData.xml.startswith('<w:drawing><wp:docPr'):
+                    field.text = "New Date"  # Change the content to a new value
+        return doc
+    except Exception as e:
+        print(f"Error: {e}")
+    
+
+def add_points(x, doc):
+    lines = x.split('\n')
+    points = []
+    current_point = ''
+    for line in lines:
+        line = line.strip()
+        if line.startswith('-'):
+            if current_point:
+                points.append(current_point.strip())
+                current_point =  ''
+        current_point += ' '+line
+    if current_point:
+        points.append(current_point.strip())
+    for i in points:
+        doc.add_paragraph(i[2:],style = 'List Bullet')
+        line_space(doc,1)
+
+def make_inspection_document(client_name, client_location, unit_number, client_code, inspection_date, equipment_name, tag_number, inspection_type, edited_df, result_and_conclusion, site_observation, overall_summary, thickness_details, scanning_details, shellwise_inspection, tower_drawing, shell_plate_pics, detailed_report):
     # Create document
     doc = docx.Document()
     paragraph_format = doc.styles['Normal'].paragraph_format
@@ -127,31 +174,46 @@ def make_inspection_document(client_name, client_location, unit_number, report_n
 
     style = doc.styles['Normal']
     font = style.font
-    font.name = 'Amasis MT Pro'
+    font.name = 'Arial'
     font.size = docx.shared.Pt(11)
 
-    # header
+    obj_styles = doc.styles
+    obj_charstyle = obj_styles.add_style('CommentsStyle', docx.enum.style.WD_STYLE_TYPE.CHARACTER)
+    obj_font = obj_charstyle.font
+    obj_font.size = Pt(16)
+    obj_font.name = 'Arial'
+
+    obj_styles = doc.styles
+    obj_charstyle = obj_styles.add_style('SmallText', docx.enum.style.WD_STYLE_TYPE.CHARACTER)
+    obj_font = obj_charstyle.font
+    obj_font.size = Pt(8)
+    obj_font.name = 'Arial'
+        # header
     section = doc.sections[0]
-    header = section.header 
+    section.different_first_page_header_footer = True
+    section.page_height = Mm(297)
+    section.page_width = Mm(210)
+    header = section.first_page_header 
     table = header.add_table(1, 3,width=Inches(8))
     hdr_cells = table.rows[0].cells
-    # add_page_number(header.paragraphs[0].add_run())
     p = hdr_cells[0].paragraphs[0] 
     format = p.paragraph_format
+    format.vertical_alignment = docx.enum.table.WD_ALIGN_VERTICAL.CENTER
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
     run = p.add_run()
-    run.add_picture(r'Logo/ArunTech.jpg', width=Inches(.7))
+    run.add_picture(r'E:\Works\GitHub\Inspection-Document-Generator\Files\Client\Logo\ArunTech.jpg', width=Inches(.7))
 
     p = hdr_cells[1].paragraphs[0] 
     format = p.paragraph_format
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-    p.add_run(f'{client_name} {client_location} {unit_number}')
+    p.add_run(f'{client_name}\n{client_location} \n UNIT: {unit_number}')
 
     p = hdr_cells[2].paragraphs[0] 
     format = p.paragraph_format
+    format.vertical_alignment = docx.enum.table.WD_ALIGN_VERTICAL.CENTER
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
     run = p.add_run()
-    run.add_picture(r'Logo/HP.png', width=Inches(1.4))
+    run.add_picture(r'E:\Works\GitHub\Inspection-Document-Generator\Files\Client\Logo\HP.png', width=Inches(0.7))
     table.style = 'Table Grid'
 
     p = header.add_paragraph()
@@ -163,59 +225,88 @@ def make_inspection_document(client_name, client_location, unit_number, report_n
     p = table_0.rows[0].cells[0].paragraphs[0]
     format = p.paragraph_format
     # format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-    p.add_run(f'Report number : {report_number}')
+    inspection_date = pd.to_datetime(inspection_date)
+    date_str = inspection_date.strftime('%B %Y/%d')
+    date_components = date_str.split(' ')
+    date_components[0] = date_components[0][:3]
+    date_components[0] = date_components[0].upper()
+    datex = ' '.join(date_components)
+    report_number = f'ATL/UT/{client_code}-{client_location}/{datex}'
+    p.add_run('Report no:\n', style = 'SmallText')
+    p.add_run(f"{report_number}")
     p = table_0.rows[0].cells[1].paragraphs[0]
     format = p.paragraph_format
+
+    inspection_dateX = inspection_date.strftime('%d-%m-%Y')
+    p.add_run('Inspection date:\n',style='SmallText')
+    p.add_run(f'{inspection_dateX}')
     # format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-    p.add_run(f'Inspection date : {inspection_date}')
-    table_0.style = 'Table Grid'
+    
+    table_0.style = 'Colorful List'
 
     font.size = docx.shared.Pt(16)
     table_1 = doc.add_table(2,1)
     p = table_1.rows[0].cells[0].paragraphs[0]
     format = p.paragraph_format
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-    p.add_run(f'{equipment_name}')
+    p.add_run(f'{equipment_name}', style = 'CommentsStyle').bold = False
     p = table_1.rows[1].cells[0].paragraphs[0]
     format = p.paragraph_format
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-    p.add_run(f'{inspection_type}')
-    table_1.style = 'Table Grid'
+    p.add_run(f'{inspection_type}', style = 'CommentsStyle').bold = True
+    table_1.style = 'Colorful List'
     font.size = docx.shared.Pt(11)
 
-    p = doc.add_paragraph()
-    p.style = None
-    p.add_run(style = None)
-    p.add_run(style = None)
-
+    line_space(doc,30)
     edited_df.to_csv(rf'Temp\authors.csv')
     authors = pd.read_csv(rf'Temp\authors.csv')
+    authors['Date'] = pd.to_datetime(authors['Date'])
+    authors['Date'] = authors['Date'].dt.strftime('%d-%m-%Y')
     authors.drop('Unnamed: 0', inplace=True, axis=1)
-    add_table_to_document(authors, doc)
+    add_table_to_document(authors, doc, 'Colorful List')
     doc.add_page_break()
-    header.is_linked_to_previous = True
 
-    section = doc.sections[-1]
+    section = doc.sections[0]
     header = section.header 
     table = header.add_table(2, 3,width=Inches(8))
     hdr_cells = table.rows[0].cells
-    # add_page_number(header.paragraphs[0].add_run())
+    
     p = hdr_cells[0].paragraphs[0] 
     format = p.paragraph_format
+    format.vertical_alignment = docx.enum.table.WD_ALIGN_VERTICAL.CENTER
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
     run = p.add_run()
-    run.add_picture(r'Logo/ArunTech.jpg', width=Inches(.7))
+    run.add_picture(r'E:\Works\GitHub\Inspection-Document-Generator\Files\Client\Logo\ArunTech.jpg', width=Inches(.7))
 
     p = hdr_cells[1].paragraphs[0] 
     format = p.paragraph_format
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-    p.add_run(f'{client_name} {client_location} {unit_number}')
+    p.add_run(f'{equipment_name}\nUNIT: {unit_number}\n')
+    p.add_run(f'(Report No. {report_number})', style = 'SmallText')
 
     p = hdr_cells[2].paragraphs[0] 
     format = p.paragraph_format
+    format.vertical_alignment = docx.enum.table.WD_ALIGN_VERTICAL.CENTER
     format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
     run = p.add_run()
-    run.add_picture(r'Logo/HP.png', width=Inches(1.4))
+    run.add_picture(r'E:\Works\GitHub\Inspection-Document-Generator\Files\Client\Logo\HP.png', width=Inches(.7))
+
+    sec_cells = table.rows[1].cells
+    p = sec_cells[0].paragraphs[0] 
+    format = p.paragraph_format
+    format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    p.add_run(f"Client:\n{client_code}-{unit_number}\n{client_location}")
+
+    p = sec_cells[1].paragraphs[0] 
+    format = p.paragraph_format
+    format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    p.add_run(f"{inspection_type}")
+
+    p = sec_cells[2].paragraphs[0] 
+    format = p.paragraph_format
+    format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    add_page_number(p.add_run())
+
     table.style = 'Table Grid'
 
     p = header.add_paragraph()
@@ -223,37 +314,57 @@ def make_inspection_document(client_name, client_location, unit_number, report_n
     p.add_run(style = None)
     p.add_run(style = None)
 
-    doc.add_paragraph('Table of contents')
+    p = doc.add_paragraph()
+    p.add_run('Table of contents', style = 'CommentsStyle').bold = True
+    format = p.paragraph_format
+    format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    
     doc = table_of_contents(doc)
     doc.add_page_break()
     hn = '1'
-    doc.add_heading(f'{hn} Results and conclusion',1)
-    doc.add_paragraph(result_and_conclusion)
-    hn = ahn(hn,1)
-    doc.add_heading(f'{hn} Site observation',1)
-    add_site_observation_to_doc(site_observation,doc,hn)
+    doc.add_heading(f'{hn}. Results and conclusion',1)
+    line_space(doc,1)
 
-    if overall_summary is not None and len(overall_summary)>0:
+    paragraph_format = doc.styles['List Bullet'].paragraph_format
+    paragraph_format.left_indent = Inches(0.5)
+
+    add_points(result_and_conclusion, doc)
+    doc.add_page_break()
+
+    hn = ahn(hn,1)
+    doc.add_heading(f'{hn}. Site observation',1)
+    line_space(doc,1)
+    add_site_observation_to_doc(site_observation,doc,hn)
+    doc.add_page_break()
+
+    if bool(overall_summary)==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Overall Summary',1)
+        doc.add_heading(f'{hn}. Overall Summary',1)
+        line_space(doc,1)
         overall_summary_df = pd.read_csv(overall_summary,encoding='utf-8')
         add_table_to_document(overall_summary_df,doc)
+        doc.add_page_break()
 
-    if thickness_details is not None and len(thickness_details)>0:
+    if bool(thickness_details)==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Thickness Details',1)
+        doc.add_heading(f'{hn}. Thickness Details',1)
+        line_space(doc,1)
         overall_summary_df = pd.read_csv(thickness_details)
         add_table_to_document(overall_summary_df,doc)
-    
-    if scanning_details is not None and len(scanning_details)>0:
+        doc.add_page_break()
+
+    if bool(scanning_details)==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Scanning Details',1)
+        doc.add_heading(f'{hn}. Scanning Details',1)
+        line_space(doc,1)
         overall_summary_df = pd.read_csv(scanning_details)
         add_table_to_document(overall_summary_df,doc)
+        doc.add_page_break()
 
-    if shellwise_inspection is not None and len(shellwise_inspection)>0:
+    if bool(shellwise_inspection)==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Shellwise Inspection',1)
+        doc.add_heading(f'{hn}. Shellwise Inspection',1)
+        line_space(doc,1)
         for table in shellwise_inspection:
             overall_summary_df = pd.read_csv(table)
             add_table_to_document(overall_summary_df,doc)
@@ -261,10 +372,12 @@ def make_inspection_document(client_name, client_location, unit_number, report_n
             p.style = None
             p.add_run(style = None)
             p.add_run(style = None)
+        doc.add_page_break()
 
-    if tower_drawing is not None and len(tower_drawing)>0:
+    if bool(tower_drawing)==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Tower Drawings',1)
+        doc.add_heading(f'{hn}. Tower Drawings',1)
+        line_space(doc,1)
         for pic in tower_drawing:
             image = Image.open(pic)
             image.save(rf'Temp\img.png')
@@ -273,10 +386,12 @@ def make_inspection_document(client_name, client_location, unit_number, report_n
             p.style = None
             p.add_run(style = None)
             p.add_run(style = None)
+        doc.add_page_break()
 
-    if shell_plate_pics is not None and len(shell_plate_pics)>0:
+    if bool(shell_plate_pics)==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Shellplate Pictures',1)
+        doc.add_heading(f'{hn}. Shellplate Pictures',1)
+        line_space(doc,1)
         for pic in shell_plate_pics:
             image = Image.open(pic)
             image.save(rf'Temp\img.png')
@@ -285,16 +400,22 @@ def make_inspection_document(client_name, client_location, unit_number, report_n
             p.style = None
             p.add_run(style = None)
             p.add_run(style = None)
+        doc.add_page_break()
     
-    if detailed_report is not None and len(detailed_report)>0:
+    if bool(list(detailed_report.values())[0])==True:
         hn = ahn(hn,1)
-        doc.add_heading(f'{hn} Detailed Report',1)
-        for table in detailed_report:
-            overall_summary_df = pd.read_csv(table)
-            add_table_to_document(overall_summary_df,doc)
-            p = doc.add_paragraph()
-            p.style = None
-            p.add_run(style = None)
-            p.add_run(style = None)
-    
+        doc.add_heading(f'{hn}. Detailed Report',1)
+        for k,v in detailed_report.items():
+            hn = ahn(hn,2)
+            doc.add_heading(f'{hn}. {k}',2)
+            line_space(doc,1)
+            for table in v:
+                overall_summary_df = pd.read_csv(table)
+                add_table_to_document(overall_summary_df,doc)
+                p = doc.add_paragraph()
+                p.style = None
+                p.add_run(style = None)
+                p.add_run(style = None)
+
+    doc = update_docx_fields(doc)
     return doc
